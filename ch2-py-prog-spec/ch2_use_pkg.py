1ea5dc9be7da998d1f730cf1e15672ff3a32b1f6
Yaakov Azat <yaakovazat@gmail.com>
2021-12-18 00:10:32 +0600

    updated the content of the chapter 2 and we are almost done.

--- ch2-py-prog-spec/ch2_use_pkg.py
+++ ch2-py-prog-spec/ch2_use_pkg.py
@@ -1,3 +1,27 @@
 import datetime
 
+import asyncio
+
 print(datetime.date.today())
+
+
+async def main():
+    print('Hello ...')
+    await asyncio.sleep(2)
+    print('....world!')
+
+
+async def printer(name: str, times: int) -> None:
+    for i in range(times):
+        print(name)
+        await asyncio.sleep(1)
+
+
+async def again():
+    await asyncio.gather(
+        printer('A', 3),
+        printer('B', 10),
+    )
+
+
+asyncio.run(again())
